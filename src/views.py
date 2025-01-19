68f0704c71d3175a248bb7cf220b6dc9f69251b2
Ilia Ryutin <iryutin@mail.ru>
2025-01-19 13:56:35 +0300

    начало

--- src/views.py
+++ src/views.py
@@ -14,16 +14,15 @@ def greeting (time:int) -> str:
     elif time in [0, 6]:
         return 'Доброй ночи'
 
-def cards(df_data_operations, date_now) -> list[dict]:
+def cards(df_data_operations, date_now):
     """Принемает датафрем фильтрует по дате и списанию затем выдаёт словарь с суммой расходов по картам"""
-    print(df_data_operations)
+
     date_beginning = date_now.replace(day=1, hour=0, minute = 0, second = 0)
     df_data_operations_by_date = df_data_operations[(date_now.strftime("%d.%m.%Y %H:%M:%S")>df_data_operations['Дата операции']) & (df_data_operations['Дата операции']>date_beginning.strftime("%d.%m.%Y %H:%M:%S"))]
-    print(df_data_operations_by_date)
     df_data_operations_pay = df_data_operations_by_date[df_data_operations_by_date['Сумма операции']<0]
     df_data_operations_pay = df_data_operations_pay.groupby('Номер карты').sum()
     df_data_operations_pay = df_data_operations_pay.loc[:,['Сумма операции']]
-    print(df_data_operations_pay)
+    return df_data_operations_pay
 
 file = os.getenv('DATA_FILE')
 print(file)
