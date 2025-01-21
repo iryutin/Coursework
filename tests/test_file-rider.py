8f06b2d7d6a64da185dfd951a7b2e4466cea195e
Ilia Ryutin <iryutin@mail.ru>
2025-01-21 19:26:20 +0300

    Добавленны тесты file_rider

---
+++ tests/test_file-rider.py
@@ -0,0 +1,36 @@
+from src.file_rider import excel_file_reader
+from unittest.mock import patch
+
+import pandas as pd
+
+@patch("pandas.read_excel")
+def test_excel_file_reader(mock_get):
+    """Тест функции excel_file_reader"""
+    mock_get.return_value = pd.DataFrame(
+        {
+            "id": 441945886,
+            "state": ["EXECUTED"],
+            "date": ["2019-08-26T10:50:58.294041"],
+            "amount": ["31957.58"],
+            "currency_name": ["руб."],
+            "currency_code": ["RUB"],
+            "description": ["Перевод организации"],
+            "from": ["Maestro 1596837868705199"],
+            "to": ["Счет 64686473678894779589"],
+        }
+    )
+    test_df = pd.DataFrame(
+        {
+            "id": 441945886,
+            "state": ["EXECUTED"],
+            "date": ["2019-08-26T10:50:58.294041"],
+            "amount": ["31957.58"],
+            "currency_name": ["руб."],
+            "currency_code": ["RUB"],
+            "description": ["Перевод организации"],
+            "from": ["Maestro 1596837868705199"],
+            "to": ["Счет 64686473678894779589"],
+        }
+    )
+    df = excel_file_reader('')
+    assert df.equals(test_df)
